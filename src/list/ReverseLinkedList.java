85d11b13442896df2d9bde5f56f133696e805914
huangfen <huangfen@baidu.com>
2020-03-29 23:00:53 +0800

    LinkedList: reverse linked list

---
+++ src/list/ReverseLinkedList.java
@@ -0,0 +1,78 @@
+package list;
+
+/**
+ * Reverse a singly linked list.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ *
+ * @author huangfen@baidu.com
+ * @create 2020-03-29 22:12
+ **/
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        // init list
+        ListNode head = new ListNode(1);
+        ListNode temp = head;
+        for (int i = 2; i <= 10; i++) {
+            ListNode node = new ListNode(i);
+            temp.next = node;
+            temp = node;
+        }
+
+        // reverse list
+        ListNode newHead = new Solution1().reverseList(head);
+
+        // print list
+        temp = newHead;
+        while (null != temp) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
+
+    private static class Solution2 {
+        public ListNode reverseList(ListNode head) {
+
+            if (null == head || null == head.next) {
+                return head;
+            }
+            ListNode pre = null;
+            ListNode cur = head;
+            while (null != cur) {
+                ListNode temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            return pre;
+        }
+    }
+
+    private static class Solution1 {
+        public ListNode reverseList(ListNode head) {
+            return swapNewHead(head, null);
+        }
+
+        private ListNode swapNewHead(ListNode head, ListNode newHead) {
+            if (null == head) {
+                return newHead;
+            }
+            ListNode next = head.next;
+            head.next = newHead;
+            return swapNewHead(next, head);
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
